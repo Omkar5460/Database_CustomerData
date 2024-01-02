37bf7fafc01edf58f6ca77e686363203bb137825
Omkar Jadhav <126196217+Omkar5460@users.noreply.github.com>
2024-01-02 12:10:02 +0530

    Cust_Trans.sql

--- Demo12.sql
+++ Demo12.sql
@@ -13,16 +13,3 @@ trans_amt int ,
 trans_date date,
 cust_id int references customer(cust_id) 
 );
-
-insert into customer values(11,"Omkar","Pune", 54321,"saving"),
-(12,"Ajinkya","Wagholi", 12345,"current"),
-(13,"Jaydeep","Solapur", 78965,"zero balance"),
-(14,"Suraj","Mumbai", 76543,"saving");
-
-insert into transactions values(01,1200,'2023-01-02',12);
-insert into transactions values(02,1500,'2023-01-12',11),(03,1900,'2023-02-02',14),
-(04,2100,'2023-02-08',13),(05,1700,'2023-02-09',11),
-(06,2500,'2023-01-02',14),(07,3200,'2023-03-09',12);
-
-
-select*from customer;
